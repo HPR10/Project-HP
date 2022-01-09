fce9e5abb7eb537011406eeeff6cf59addee8b77
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-01-08 23:31:22 -0300

    Initial Commit

---
+++ Project HP/Project_HPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_HPApp.swift
+//  Project HP
+//
+//  Created by Hugo Pinheiro on 08/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_HPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
